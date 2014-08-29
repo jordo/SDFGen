3d3abe20c5783dcd2a5747cad982aec5d1ddf6d2
Oleg Osin <oleg@apportable.com>
2014-08-28 19:19:41 -0700

    parralalized distance field generation.

--- SDFGen/AppDelegate.m
+++ SDFGen/AppDelegate.m
@@ -21,7 +21,6 @@ static CGPoint s_outside = {9999, 9999};
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
     [self loadInputImage];
 
     _spread.delegate = self;
@@ -110,47 +109,46 @@ static CGPoint compare(CGPoint** grid, CGPoint cell, int x, int y, int x2, int y
 
 static void fillDistance(CGPoint** grid, int width, int height)
 {
-    CGPoint point;
-    for(int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
+    dispatch_queue_priority_t prio = DISPATCH_QUEUE_PRIORITY_DEFAULT;
+    dispatch_apply(height, dispatch_get_global_queue(prio, 0), ^(size_t y) {
+        dispatch_apply(width, dispatch_get_global_queue(prio, 0), ^(size_t x) {
+            CGPoint point;
             point = grid[y][x];
             point = compare(grid, point, x, y, -1, 0);
             point = compare(grid, point, x, y, 0, -1);
             point = compare(grid, point, x, y, -1, -1);
             point = compare(grid, point, x, y, 1, -1);
             grid[y][x] = point;
-        }
-        
-        for(int x = width - 1; x >= 0; x--)
-        {
+        });
+        dispatch_apply(width, dispatch_get_global_queue(prio, 0), ^(size_t x_inv) {
+            CGPoint point;
+            int x = width - x_inv;
             point = grid[y][x];
             point = compare(grid, point, x, y, 1, 0);
             grid[y][x] = point;
-        }
-    }
+        });
+    });
     
-    for (int y = height - 1; y >= 0; y--)
-    {
-        for (int x = width - 1; x >= 0; x--)
-        {
+    
+    dispatch_apply(height, dispatch_get_global_queue(prio, 0), ^(size_t y) {
+        dispatch_apply(width, dispatch_get_global_queue(prio, 0), ^(size_t x_inv) {
+            CGPoint point;
+            int x = width - x_inv;
             point = grid[y][x];
             point = compare(grid, point, x, y, 1, 0);
             point = compare(grid, point, x, y, 0, 1);
             point = compare(grid, point, x, y, -1, 1);
             point = compare(grid, point, x, y, 1, 1);
             grid[y][x] = point;
-        }
+        });
         
-        for (int x = 0; x < width; x++)
-        {
+        dispatch_apply(width, dispatch_get_global_queue(prio, 0), ^(size_t x) {
+            CGPoint point;
             point = grid[y][x];
             point = compare(grid, point, x, y, -1, 0);
             grid[y][x] = point;
-        }
-    }
-    
+        });
+    });
 }
 
 static int npot(int n)
@@ -169,12 +167,6 @@ static int npot(int n)
 
 - (BOOL)control:(NSControl *)control textShouldBeginEditing:(NSText *)fieldEditor
 {
-    NSString* curtxt = fieldEditor.string;
-    
-    NSCharacterSet* decimals = [NSCharacterSet decimalDigitCharacterSet];
-    
-    NSRange wtf = [curtxt rangeOfCharacterFromSet:decimals];
-
     return YES;
 }
 
@@ -206,6 +198,9 @@ static int npot(int n)
     
     if(inputBitmapRep == nil)
         return;
+
+    _generateButton.enabled = NO;
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     
     // create dst image with adjusted size
     // create 2 grid representation for each input/output image
@@ -221,121 +216,162 @@ static int npot(int n)
     
     s_width = (int)offscreenRect.size.width;
     s_height = (int)offscreenRect.size.width;
-    
+
     // create grid1
-    CGPoint** grid1;
+    __block CGPoint** grid1;
     grid1 = (CGPoint**) malloc(s_height*sizeof(CGPoint*));
-    for(int i = 0; i < s_height; i++)
-    {
-        grid1[i] = (CGPoint*)malloc(s_width*sizeof(CGPoint));
-    }
-    
-    CGPoint** grid2;
+    __block CGPoint** grid2;
     grid2 = (CGPoint**) malloc(s_height*sizeof(CGPoint*));
-    for(int i = 0; i < s_height; i++)
-    {
-        grid2[i] = (CGPoint*)malloc(s_width*sizeof(CGPoint));
-    }
     
-
-    // fill out the grids
-    for(int y = 0; y < s_height; y++)
-    {
-        for(int x = 0; x < s_width; x++)
+    void (^finalize)(void) = ^{
+        static int s_distancesFilled = 0;
+        s_distancesFilled++;
+        
+        if(s_distancesFilled >= 2)
         {
-            NSColor* inputColor = [inputBitmapRep colorAtX:x y:y];
-            int pixel = (int)(inputColor.redComponent * 255.0f);
-            grid1[y][x] = cell(pixel);
-            grid2[y][x] = cell(invertCell(pixel));
+            NSLog(@"distances generated, normalize and save output");
+            // create distance field
+            float** distanceField;
+            distanceField = (float**) malloc(s_height*sizeof(float*));
+            for(int i = 0; i < s_height; i++)
+            {
+                distanceField[i] = (float*) malloc(s_width*sizeof(float));
+            }
+            
+            float dist1;
+            float dist2;
+            // fill out the grids
+            for(int y = 0; y < s_height; y++)
+            {
+                for(int x = 0; x < s_width; x++)
+                {
+                    dist1 = sqrtf(distSquared(grid1[y][x]));
+                    dist2 = sqrtf(distSquared(grid2[y][x]));
+                    distanceField[y][x] = dist1 - dist2;
+                }
+            }
+            
+            // create output image
+            NSBitmapImageRep* offscreenRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:nil
+                                                                                     pixelsWide:offscreenRect.size.width
+                                                                                     pixelsHigh:offscreenRect.size.height
+                                                                                  bitsPerSample:8
+                                                                                samplesPerPixel:4
+                                                                                       hasAlpha:YES
+                                                                                       isPlanar:NO
+                                                                                 colorSpaceName:NSCalibratedRGBColorSpace
+                                                                                   bitmapFormat:0
+                                                                                    bytesPerRow:(4 * offscreenRect.size.width)
+                                                                                   bitsPerPixel:32];
+            
+            // normalize
+            float dist = 0.0f;
+            float maxDist = spread;
+            float minDist = spread * -1;
+            float colorComponent;
+            
+            for(int y = 0; y < s_height; y++)
+            {
+                for(int x = 0; x < s_width; x++)
+                {
+                    dist = distanceField[y][x];
+                    
+                    if(dist < 0)
+                    {
+                        dist = -128 * (dist - minDist) / minDist;
+                    }
+                    else
+                    {
+                        dist = 128 + 128 * dist / maxDist;
+                    }
+                    
+                    if(dist < 0)
+                    {
+                        dist = 0;
+                    }
+                    else if(dist > 255)
+                    {
+                        dist = 255;
+                    }
+                    
+                    colorComponent = dist / 255.0f;
+                    
+                    NSColor* outputColor = [NSColor colorWithCalibratedRed:colorComponent green:colorComponent blue:colorComponent alpha:1.0];
+                    [offscreenRep setColor:outputColor atX:x y:y];
+                }
+            }
+            
+            free(grid1);
+            free(grid2);
+            free(distanceField);
+            
+            _outputImage = [[NSImage alloc] initWithCGImage:[offscreenRep CGImage] size:offscreenRect.size];
+            int outputWidth = _width.intValue;
+            int outputHeight = _height.intValue;
+            _outputImage = [self imageResize:_outputImage newSize:CGSizeMake(npot(outputWidth),
+                                                                             npot(outputHeight))];
+            
+            [_outputImageView setImage:_outputImage];
+            
+            [self saveImageToFile:_outputImage name:@"output.png"];
+            _generateButton.enabled = YES;
         }
-    }
+        
+    };
     
-    fillDistance(grid1, s_width, s_height);
-    fillDistance(grid2, s_width, s_height);
-    
-    // create distance field
-    float** distanceField;
-    distanceField = (float**) malloc(s_height*sizeof(float*));
-    for(int i = 0; i < s_height; i++)
-    {
-        distanceField[i] = (float*) malloc(s_width*sizeof(float));
-    }
-    
-    // fill out the grids
-    for(int y = 0; y < s_height; y++)
-    {
-        for(int x = 0; x < s_width; x++)
+    void (^fillGrids)(void) = ^{
+        static int s_gencnt = 0;
+        s_gencnt++;
+        if(s_gencnt >= 2)
         {
-            float dist1 = sqrtf(distSquared(grid1[y][x]));
-            float dist2 = sqrtf(distSquared(grid2[y][x]));
-            distanceField[y][x] = dist1 - dist2;
+            NSLog(@"filling grids");
+            NSColor* inputColor;
+            int pixel;
+            // fill out the grids
+            for(int y = 0; y < s_height; y++)
+            {
+                for(int x = 0; x < s_width; x++)
+                {
+                    inputColor = [inputBitmapRep colorAtX:x y:y];
+                    pixel = (int)(inputColor.redComponent * 255.0f);
+                    grid1[y][x] = cell(pixel);
+                    grid2[y][x] = cell(invertCell(pixel));
+                }
+            }
+            
+            NSLog(@"done filling grid");
+            // start distance generation
+            dispatch_async(globalQueue, ^{
+                NSLog(@"start distance 1");
+                fillDistance(grid1, s_width, s_height);
+                finalize();
+            });
+            
+            dispatch_async(globalQueue, ^{
+                NSLog(@"start distance 2");
+                fillDistance(grid2, s_width, s_height);
+                finalize();
+            });
         }
-    }
+    };
     
-    // create output image
-    NSBitmapImageRep* offscreenRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:nil
-                                                           pixelsWide:offscreenRect.size.width
-                                                           pixelsHigh:offscreenRect.size.height
-                                                        bitsPerSample:8
-                                                      samplesPerPixel:4
-                                                             hasAlpha:YES
-                                                             isPlanar:NO
-                                                       colorSpaceName:NSCalibratedRGBColorSpace
-                                                         bitmapFormat:0
-                                                          bytesPerRow:(4 * offscreenRect.size.width)
-                                                         bitsPerPixel:32];
-
-    
-    // normalize
-    float dist = 0.0f;
-    float maxDist = spread;
-    float minDist = spread * -1;
-    
-    for(int y = 0; y < s_height; y++)
-    {
-        for(int x = 0; x < s_width; x++)
+    dispatch_async(globalQueue, ^{
+        for(int i = 0; i < s_height; i++)
         {
-            dist = distanceField[y][x];
-            
-            if(dist < 0)
-            {
-                dist = -128 * (dist - minDist) / minDist;
-            }
-            else
-            {
-                dist = 128 + 128 * dist / maxDist;
-            }
-            
-            if(dist < 0)
-            {
-                dist = 0;
-            }
-            else if(dist > 255)
-            {
-                dist = 255;
-            }
-            
-            float colorComponent = dist / 255.0f;
-            
-            NSColor* outputColor = [NSColor colorWithCalibratedRed:colorComponent green:colorComponent blue:colorComponent alpha:1.0];
-            [offscreenRep setColor:outputColor atX:x y:y];
+            grid1[i] = (CGPoint*)malloc(s_width*sizeof(CGPoint));
         }
-    }
+        NSLog(@"done grid 1");
+        fillGrids();
+    });
     
-    free(grid1);
-    free(grid2);
-    free(distanceField);
-    
-    _outputImage = [[NSImage alloc] initWithCGImage:[offscreenRep CGImage] size:offscreenRect.size];
-    int outputWidth = _width.intValue;
-    int outputHeight = _height.intValue;
-    _outputImage = [self imageResize:_outputImage newSize:CGSizeMake(npot(outputWidth),
-                                                                     npot(outputHeight))];
-    
-    [_outputImageView setImage:_outputImage];
-    
-    [self saveImageToFile:_outputImage name:@"output.png"];
-
+    dispatch_async(globalQueue, ^{
+        for(int i = 0; i < s_height; i++)
+        {
+            grid2[i] = (CGPoint*)malloc(s_width*sizeof(CGPoint));
+        }
+        NSLog(@"done grid 2");
+        fillGrids();
+    });
 }
 
 @end
